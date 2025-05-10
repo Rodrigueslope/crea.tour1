c5e472403cf86ce7dd54d546b9807d5c1ddf8877
Rodrigueslope <consultoriarbim@gmail.com>
2025-05-10 17:04:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const telhadoID = "cc4f5c1c944da13c6909f46edcefc422";
 const API_URL = "https://api.open-meteo.com/v1/forecast?latitude=-10.9472&longitude=-37.0731&current=temperature_2m,relative_humidity_2m";
 
 async function atualizarDadosClimaticos() {
@@ -8,13 +9,15 @@ async function atualizarDadosClimaticos() {
     const temperatura = data.current.temperature_2m;
     const umidade = data.current.relative_humidity_2m;
 
-    document.getElementById("temp-value").innerText = temperatura + " °C";
-    document.getElementById("hum-value").innerText = umidade + " %";
+    document.getElementById("temp-value").innerText = `${temperatura} °C`;
+    document.getElementById("hum-value").innerText = `${umidade} %`;
 
     const iframe = document.getElementById("speckle-container");
     const viewer = iframe?.contentWindow?.document;
 
-    const telhado = viewer?.querySelector('[data-object-id="cc4f5c1c944da13c6909f46edcefc422"]');
+    if (!viewer) return;
+
+    const telhado = viewer.querySelector(`[data-object-id='${telhadoID}']`);
     if (telhado) {
       if (temperatura > 25) {
         telhado.classList.add("blinking");
